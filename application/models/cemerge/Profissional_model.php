928ca60ef6c62fb472e47263e205d911ee5429da
Anderson <anderson@archlinux.com.br>
2021-03-03 16:45:58 -0300

    Revert "Merge branch 'master' of https://github.com/andersonpiano/sgc"
    This reverts commit 0ef1f8fd88c5dee3e232a2f3b7ba8f82499b06cd, reversing
    changes made to 702445eb7a41cfcb339ef27d4692aebd4c1a26ab.

--- application/models/cemerge/Profissional_model.php
+++ application/models/cemerge/Profissional_model.php
@@ -74,9 +74,7 @@ class Profissional_model extends MY_Model
     }
 
     var $column_search = array("nome");
-
-    var $column_order = array("id", "nome", "registro", "email");
-
+    var $column_order = array("id", "nome", "especializacao");
 
     private function _get_datatable() {
 
