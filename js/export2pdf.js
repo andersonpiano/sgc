837892d00c9e52126b5b6c7af42717aed9f3a849
FernandoSFreire <73886844+FernandoSFreire@users.noreply.github.com>
2021-03-10 15:41:00 -0300

    escala
    adicionar médico e busca de escala

--- js/export2pdf.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function(){
-    $(document).on('click', '#bt_gerar_pdf', function() {
-        var doc = new jsPDF('landscape', 'pt', 'a4');
-        doc.fromHTML($('#wrapper'), function() {
-            doc.save("SGC.pdf");
-        });
-    });
-});
