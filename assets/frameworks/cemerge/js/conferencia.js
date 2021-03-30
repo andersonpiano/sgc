1a98bc8c8f41c57ad77b0b7cd54e0e3712e91e7c
andersonpiano <anderson@archlinux.com.br>
2021-03-30 11:15:24 -0300

    Update conferencia.js

--- assets/frameworks/cemerge/js/conferencia.js
+++ assets/frameworks/cemerge/js/conferencia.js
@@ -45,7 +45,7 @@ $(function(){
 					success: function(responseData){
 						if (JSON.parse(responseData).sucess){
 						//swal('Sucesso','Médico Excluido com sucesso','success');
-						await new Promise(r => setTimeout(r, 2000));
+						//await new Promise(r => setTimeout(r, 2000));
 						document.location.reload(true);
 						} else {
 							swal('Erro','Este plantão foi recebido por cessão/troca e não pode ser removido aqui.','error');
